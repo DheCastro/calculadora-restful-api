331949acca17d92b52f6b4560ddc91343d117139
Dhellano de Castro Oliveira <dhecastro90@gmail.com>
2021-01-04 11:32:27 -0300

    add subtraction, multiplication, division, average and squareroot operations

--- src/main/java/br/com/dhecastro/calculadorarestfulapi/controller/MathController.java
+++ src/main/java/br/com/dhecastro/calculadorarestfulapi/controller/MathController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.dhecastro.calculadorarestfulapi.exceptions.DivisionForZeroException;
 import br.com.dhecastro.calculadorarestfulapi.exceptions.UnsupportedMathOperationsException;
+import br.com.dhecastro.calculadorarestfulapi.util.CalculadoraUtil;
 
 @RestController
 public class MathController {
@@ -13,24 +15,73 @@ public class MathController {
 	@RequestMapping(value="/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
 	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
 	
-		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		if(!CalculadoraUtil.isNumeric(numberOne) || !CalculadoraUtil.isNumeric(numberTwo)) {
 			throw new UnsupportedMathOperationsException("Please set a numeric value");
 		}
 		
-		Double sum = convertToDouble(numberOne) + convertToDouble(numberTwo);
+		Double sum = CalculadoraUtil.convertToDouble(numberOne) + CalculadoraUtil.convertToDouble(numberTwo);
 		return sum;
 	}
-
-	private Double convertToDouble(String strNumber) {
-		if(strNumber == null) return 0D;
-		String number = strNumber.replaceAll(",", ".");
-		if(isNumeric(number)) return Double.parseDouble(number);
-		return 0D;
+	
+	@RequestMapping(value="/sub/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double sub(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+	
+		if(!CalculadoraUtil.isNumeric(numberOne) || !CalculadoraUtil.isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationsException("Please set a numeric value");
+		}
+		
+		Double sub = CalculadoraUtil.convertToDouble(numberOne) - CalculadoraUtil.convertToDouble(numberTwo);
+		return sub;
+	}
+	
+	@RequestMapping(value="/mult/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double mult(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+	
+		if(!CalculadoraUtil.isNumeric(numberOne) || !CalculadoraUtil.isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationsException("Please set a numeric value");
+		}
+		
+		Double mult = CalculadoraUtil.convertToDouble(numberOne) * CalculadoraUtil.convertToDouble(numberTwo);
+		return mult;
+	}
+	
+	@RequestMapping(value="/div/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double div(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+	
+		if(!CalculadoraUtil.isNumeric(numberOne) || !CalculadoraUtil.isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationsException("Please set a numeric value");
+		}
+		
+		if(CalculadoraUtil.isNumeric(numberTwo)) {
+			if(CalculadoraUtil.convertToDouble(numberTwo) == 0D) {
+				throw new DivisionForZeroException("Division for zero not permited");
+			}
+		}
+		
+		Double div = CalculadoraUtil.convertToDouble(numberOne) / CalculadoraUtil.convertToDouble(numberTwo);
+		return div;
+	}
+	
+	@RequestMapping(value="/sqrt/{numberOne}", method=RequestMethod.GET)
+	public Double sqrt(@PathVariable("numberOne") String numberOne) throws Exception {
+	
+		if(!CalculadoraUtil.isNumeric(numberOne)) {
+			throw new UnsupportedMathOperationsException("Please set a numeric value");
+		}
+		
+		Double sqrt = Math.sqrt(CalculadoraUtil.convertToDouble(numberOne));
+		return sqrt;
 	}
 
-	private boolean isNumeric(String strNumber) {
-		if(strNumber == null) return false;
-		String number = strNumber.replaceAll(",", ".");
-		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+	@RequestMapping(value="/med/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double med(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+	
+		if(!CalculadoraUtil.isNumeric(numberOne) || !CalculadoraUtil.isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationsException("Please set a numeric value");
+		}
+		
+		Double sum = CalculadoraUtil.convertToDouble(numberOne) + CalculadoraUtil.convertToDouble(numberTwo);
+		
+		return sum/2;
 	}
 }
--- src/main/java/br/com/dhecastro/calculadorarestfulapi/handler/CustomizeResponseEntityExceptionHandler.java
+++ src/main/java/br/com/dhecastro/calculadorarestfulapi/handler/CustomizeResponseEntityExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import br.com.dhecastro.calculadorarestfulapi.exceptions.DivisionForZeroException;
 import br.com.dhecastro.calculadorarestfulapi.exceptions.ExceptionResponse;
 import br.com.dhecastro.calculadorarestfulapi.exceptions.UnsupportedMathOperationsException;
 
@@ -23,7 +24,7 @@ public class CustomizeResponseEntityExceptionHandler extends ResponseEntityExcep
 		return new ResponseEntity<>(exception, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
-	@ExceptionHandler(UnsupportedMathOperationsException.class)
+	@ExceptionHandler({UnsupportedMathOperationsException.class, DivisionForZeroException.class})
 	public final ResponseEntity<ExceptionResponse> handlerBadRequestException(Exception ex, WebRequest request){
 		ExceptionResponse exception = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 		return new ResponseEntity<>(exception, HttpStatus.BAD_REQUEST);
